dbcfcd1dd63e4744904e46438e2583872a6a0b75
aniketmdinde <aniketmdinde@gmail.com>
2025-02-01 16:43:12 +0530

    Stable commit 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Map from './components/Map';
-=======
-import { useState } from 'react'
-import Map from './components/Map'
-import TestBackend from './components/TestBackend'
->>>>>>> e00224b269bc87edfc3a22518163b2748a7030f8
 import './App.css'
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
@@ -19,15 +12,6 @@ function App() {
       </Routes>
     </Router>
   );
-=======
-    <>
-      <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
-        <Map />
-        <TestBackend />
-      </div>
-    </>
-  )
->>>>>>> e00224b269bc87edfc3a22518163b2748a7030f8
 }
 
 export default App;
